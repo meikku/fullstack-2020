bdb4922debec4f1d8023391756a9a7625dd952f1
meikku <59999917+meikku@users.noreply.github.com>
2020-09-23 20:57:27 +0300

    unicafe  1.11. fixed

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -47,11 +47,7 @@ const App = () => {
 
   return (
     <div>
-      <Statistic text="good" value ={props.good} />
-      <Statistic text="neutral" value ={props.neutral} />
-      <Statistic text="bad" value ={props.bad} />
-      <Statistic text="all" value ={props.all} />
-      <Statistic text="average" value ={props.average} />
+      
       <table>
         <tbody>
         <tr>
