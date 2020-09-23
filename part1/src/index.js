2345eadaf5ec8a8c2d630ad9ecce285da6106479
meikku <59999917+meikku@users.noreply.github.com>
2020-09-23 09:10:36 +0300

    1.12 under construction

--- part1/src/index.js
+++ part1/src/index.js
@@ -1,90 +1,31 @@
 import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
+const App = (props) => {
+  const [selected, setSelected] = useState(0)
+  const handleClick = () => {
+    console.log('setting Selected')
+    setSelected(2)
+    console.log(selected)
+  }
 
-const App = () => {
-  // save clicks of each button to own state
-  const [good, setGood] = useState(0)
-  
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
-  const all = good + bad + neutral
-  const average = (good - bad)/all
-  const positive = good / all
-  
   return (
     <div>
-      <h1>Please give us your feedback!</h1>
-      <Button handleClick = {() => setGood(good + 1)}
-        text="good"/>
-      <Button handleClick = {() => setNeutral(neutral + 1)}
-        text="neutral"/>
-      <Button handleClick = {() => setBad(bad + 1)}
-        text="bad" />   
-      <h1>statistics</h1>
-      <Statistics all={all} good={good} neutral={neutral} bad={bad} average={average} positive={positive}/>
+      <button onClick={props.anecdotes[selected]}>button</button>
     </div>
   )
-  }
-  const Button = (props) => {
-    console.log(props)
-    return(
-        <button onClick={props.handleClick}>
-          {props.text}
-        </button>
-    )
-  }
-  const Statistics = (props) => {
-    if (props.all=== 0) {
-      return (     
-        <div>
-          <p>No feedback given</p>
-        </div>
-      )
-      }
-  
-  return (
-    <div>
-      <table>
-        <tbody>
-        <tr>
-            <td><Statistic text="good"/></td>
-            <td> <Statistic value ={props.good}/> </td>
-        </tr>
-        <tr>
-            <td><Statistic text="neutral" /></td>
-            <td><Statistic value ={props.neutral} /></td>
-        </tr>
-        <tr>
-          <td><Statistic text="bad"/></td>
-          <td><Statistic value ={props.bad} /></td>
-        </tr>
-       <tr>
-         <td> <Statistic text="all" /></td>
-         <td> <Statistic value ={props.all} /></td>
-       </tr>
-       <tr>
-         <td><Statistic text="average" /></td>
-         <td><Statistic value ={props.average} /></td>
-       </tr>
-       </tbody>
-    </table>
-    
-     
-     
-      
-      <Statistic text="positive" value ={props.positive} />
-    </div>
-    )
-  }
-  const Statistic = (props) => {
-    return (
-      <div>
-  {props.text}{" "}{props.value}
-      </div>
-    )
-  }
-   
-ReactDOM.render(<App />, 
+}
+
+const anecdotes = [
+  'If it hurts, do it more often',
+  'Adding manpower to a late software project makes it later!',
+  'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+  'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+  'Premature optimization is the root of all evil.',
+  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
+]
+
+ReactDOM.render(
+  <App anecdotes={anecdotes} />,
   document.getElementById('root')
 )
