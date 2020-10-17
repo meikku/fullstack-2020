7fb17d68dec859e1dc5b273f48e8269d32bf0a66
meikku <59999917+meikku@users.noreply.github.com>
2020-10-17 12:22:04 +0300

    ex 2.12 done

---
+++ part2/dataforcountries/src/App.js
@@ -0,0 +1,100 @@
+
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+
+const App = () => {
+  const [ countries, setCountries ] = useState([ ])
+  const [ search, setSearch ] = useState('')
+
+  useEffect(() => {
+    axios
+    .get('https://restcountries.eu/rest/v2/all')
+    .then(response => {
+      console.log('promise fulfilled')
+      setCountries(response.data)
+      
+    })
+  }, [])
+  console.log('render', countries.length, 'countries')
+
+  const handleSearch = (event) => {
+      setSearch(event.target.value)
+  }
+  
+  return (
+    <div >
+      <form>
+        find countries <input country="text" onChange={handleSearch}>
+        </input>
+      </form>
+     <Countries countries={countries} search={search}/>
+    </div>
+  );
+}
+const Countries = ({ search, countries}) => {
+  const countriesToShow = 
+    countries.filter(country => country.name.toLowerCase().includes(search.toLowerCase()))
+    console.log('countries.length', countriesToShow.length)
+
+    if (countriesToShow.length > 10)
+    
+     return (
+     <div>
+       Too many matches, please specify another filter
+       </div>
+      )
+      else if (countriesToShow.length === 1)
+      return  (
+        <div>
+          <ul>
+          {countriesToShow.map(country =>  
+          <SingleCountry key={country.alpha2Code} country={country} />
+          )}
+          </ul> 
+        </div>
+       
+      )
+
+      else
+      return (
+        <div>
+          <ul>
+            {countriesToShow.map(country => 
+            <li key={country.alpha2Code}>
+              <h3>{country.name}</h3>
+              </li>
+          )}
+          </ul>
+          </div>
+          )   
+} 
+
+const SingleCountry = ( {country, flag }) => {
+  
+ return (
+   <div>
+
+  
+   <ul>
+     <h2>{country.name}</h2>
+     <p>
+     capital {country.capital}
+     </p>
+     <p>
+     population {country.population} 
+     </p>
+     <h4>languages</h4>
+     <ul>
+        {country.languages.map(language => 
+        <li key={language.iso639_1}>{language.name}</li>
+        )}
+        </ul>
+   </ul>
+   <br></br>
+   <img alt='flag' src={country.flag} width="150" height="100"></img>
+   </div>
+ )
+}
+
+
+export default App;
