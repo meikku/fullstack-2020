d0e33d2e60f23bdbb88a245941d2c12d8b22d1e6
meikku <59999917+meikku@users.noreply.github.com>
2020-11-30 18:38:55 +0200

    adding blog test: getting 201 instead of 200

---
+++ part4/tests/adding_blog.test.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+const helper = require('./listHelper')
+const initialBlogs = require('./listHelper').initialBlogs
+const app = require('../app')
+const api = supertest(app)
+const Blog = require('../models/blog')
+
+test('a valid blog can be added', async () => {
+    const newBlog = {
+        title: 'The coding genius',
+        author: 'Meikku',
+        url: 'https://meikku.com',
+        likes: 1
+    }
+
+    await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(200)
+    .expect('Content-Type', /application\/json/)
+
+    const response = await api.get('/api/blogs')
+
+    const contents = response.body.map(r => r.content)
+
+    expect(response.body).toHaveLength(initialBlogs.length + 1)
+    expect(contents).toContain(
+        'The coding genius'
+    )
+})
+
+afterAll(() => {
+    mongoose.connection.close()
+  }) 
