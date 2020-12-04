b13ec7433b3ace1c28da61de0402ba7052d9380c
Merja Pyrhönen SOFTK20P <merja.pyrhonen@centria.fi>
2020-12-04 13:55:05 +0200

    ex 4.18 done

--- part4/app.js
+++ part4/app.js
@@ -8,6 +8,7 @@ const middleware = require('./utils/middleware')
 const logger = require('./utils/logger')
 const mongoose = require('mongoose')
 const usersRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
 
 
 logger.info('connecting to', config.MONGODB_URI)
@@ -23,10 +24,12 @@ app.use(cors())
 app.use(express.json())
 app.use(middleware.requestLogger)
 
+app.use('/api/login', loginRouter)
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
+
 
 module.exports = app
