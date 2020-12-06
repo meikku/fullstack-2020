cc6c0e6dcff6e2d5061929f467809f249a463240
meikku <59999917+meikku@users.noreply.github.com>
2020-12-06 21:28:50 +0200

    add_blog.rest not working

--- part4/controllers/login.js
+++ part4/controllers/login.js
@@ -28,7 +28,7 @@ const token = jwt.sign(userForToken, process.env.SECRET)
 
 response
 .status(200)
-.send({ token, username: user.username, name: user.name})
+.send({ token, username: user.username, name: user.name })
 })
 
 module.exports = loginRouter
--- part4/utils/middleware.js
+++ part4/utils/middleware.js
@@ -1,4 +1,4 @@
-const { startsWith } = require('lodash')
+// const { startsWith } = require('lodash')
 const logger = require('./logger')
 
 const requestLogger = (request, response, next) => {
@@ -13,17 +13,6 @@ const unknownEndpoint = (request, response) => {
   response.status(404).send({ error: 'unknown endpoint' })
 }
 
-const errorHandler = (error, request, response, next) => {
-  logger.error(error.message)
-
-  if (error.name === 'CastError' && error.kind === 'ObjectId') {
-    return response.status(400).send({ error: 'malformatted id' })
-  } else if (error.name === 'ValidationError') {
-    return response.status(400).json({ error: error.message })
-  }
-
-  next(error)
-}
 
 const tokenExtractor = (request, response, next) => {
   const authorization = request.get('authorization')
@@ -32,6 +21,19 @@ const tokenExtractor = (request, response, next) => {
   }
   // code that extracts the token
   next()
+}
+
+const errorHandler = (error, request, response, next) => {
+  logger.error(error.message)
+
+  if (error.name === 'CastError' && error.kind === 'ObjectId') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
+  } else if (error.name === 'JsonWebTokenError') {
+    return response.status(401).json({ error: 'invalid token'})
+  }
+  next(error)
 }
 
 module.exports = {
