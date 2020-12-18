0e6feecf771d4d1ec68a356618ef5b57cdf59262
Merja Pyrhönen SOFTK20P <merja.pyrhonen@centria.fi>
2020-12-18 10:21:26 +0200

    ex 5.7 done

--- part5/frontend/src/components/Blog.js
+++ part5/frontend/src/components/Blog.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 const Blog = ({ blog }) => {
   const blogStyle = {
@@ -8,16 +8,24 @@ const Blog = ({ blog }) => {
     borderWidth: 1,
     marginBottom: 5
   }
+  const [detailsVisible, setDetailsVisible] = useState(false)
+  const hideWhenVisible = { display: detailsVisible ? 'none' : '' }
+  const showWhenVisible = { display: detailsVisible ? '' : 'none' }
 
   return (
     <div style={blogStyle}>
-  <div>
-    <p>{blog.title} {blog.author}</p> 
-    <p>{blog.url}</p> 
-    <p>{blog.likes}<button>like</button></p> 
-    <p>{blog.user.name}</p>
-  </div>
-  </div>
-  )}
+      <div style={hideWhenVisible}>
+        <p>{blog.title} {blog.author}<button onClick ={() => setDetailsVisible(true)}>view</button></p>
+        
+      </div>
+      <div style={showWhenVisible}>
+      <p>{blog.title} {blog.author}<button onClick ={() => setDetailsVisible(false)}>hide</button></p>
+      <p>{blog.url}</p>
+      <p>{blog.likes}<button>like</button></p>
+      <p>{blog.user.name}</p>
+      </div>
+    </div>
+  )
+}
 
 export default Blog
